293003932566f5970cb59c4842bb433e79fb6ef1
Edward Paget <ed.paget@gmail.com>
2021-02-14 12:36:28 -0600

    Add new emacs configuration

---
+++ emacs.d/init.el
@@ -0,0 +1,14 @@
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(projectile emojify emoji-mode rainbow-delimiters rainbow-delimiers rainbow-parens moe-theme lispyville lispy ivy which-key magit evil-collection evil use-package-ensure-system-package use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
