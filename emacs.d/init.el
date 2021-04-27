a385a69487562293b22447f6c25de9fd1ed7bd45
Edward Paget <ed.paget@gmail.com>
2021-04-27 16:43:03 -0500

    Update emacs configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(projectile emojify emoji-mode rainbow-delimiters rainbow-delimiers rainbow-parens moe-theme lispyville lispy ivy which-key magit evil-collection evil use-package-ensure-system-package use-package)))
+   '(flycheck exec-path-from-shell cider clojure-mode company-mode diminish projectile emojify emoji-mode rainbow-delimiters rainbow-delimiers rainbow-parens moe-theme lispyville lispy ivy which-key magit evil-collection evil use-package-ensure-system-package use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
